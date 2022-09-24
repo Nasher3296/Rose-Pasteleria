99a2a65c4deba0a0fef55a18eccf01b1223e2b25
Nasher3296 <ignaciofernandez3296@gmail.com>
2022-09-24 17:23:29 -0300

    Outstanding products

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-function scrolling() {
-    console.log(window.pageYOffset);
+$(document).ready(function () {
+    showOustandingProducts(3);
+});
+
+function showOustandingProducts(n) {
+    let arr = fetchProducts(n);
+    console.log(arr);
+    arr.forEach(e => {console.log("aa")});
+
+    $("#outstandingProductsContainer").html()
+}
+function fetchProducts(n) {
+    let response = [];
+    fetch("./db/products.JSON")
+        .then(res => res.json())
+        .then(data => {
+            [...data].sort(() => 0.5 - Math.random()).slice(0, n).forEach(e => {
+                response.push(e);
+            });
+        })
+    return response;
 }
